4a93623c027ee4cd90577498a63ab6165c9ce879
Higor Silva Rosa <higor.s.rosa@gmail.com>
2017-08-08 00:44:03 -0300

    Teste inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/primeng';
+import * as TreeModel from 'tree-model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  data: TreeNode[];
+
+  tree: any;
+
+  draggedNode;
+
+  ngOnInit() {
+    this.data = [{
+      label: 'Neo',
+      children: [
+        {
+          label: 'Diogo',
+          children: [
+            {
+              label: 'Higor',
+              children: [
+                {
+                  label: 'Angular 2'
+                },
+                {
+                  label: 'PrimeNg'
+                }
+              ]
+            },
+            {
+              label: 'Edson',
+              children: [
+                {
+                  label: 'PHP',
+                },
+                {
+                  label: 'Amazon AWS'
+                },
+                {
+                  label: 'Docker'
+                }
+              ]
+            }
+          ]
+        },
+      ]
+    }];
+
+    this.tree = new TreeModel();
+    this.tree = this.tree.parse(this.data);
+  }
+
+  dragStart(event, node) {
+    this.draggedNode = node;
+  }
+
+  dragEnd(event) {
+    this.draggedNode = null;
+  }
+
+  dropNode(event, node) {
+    console.log(this.draggedNode);
+    console.log(node);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { OrganizationChartModule, DragDropModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    OrganizationChartModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
